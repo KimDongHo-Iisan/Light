8e09f9e7b1962d7ea8223a1d19eb70037c069ad3
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-06-15 18:04:07 +0900

    Light 4

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -8,19 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var lightButton: UIButton!
     
+    // 버튼도 변경될 일이 없기 때문에 아울렛도 필요가 없다.
+    //@IBOutlet weak var lightButton: UIButton!
+    /* 아울렛을 없앨때 주의해야할것이 이렇게 그냥 없애버리게 되면 crash가 발생하게 된다.
+     setValue:forUndefinedKey:]: this class is not key value coding-compliant for the key lightButton.'
+     -> mainStoryBoard에서의 viewController에서는 라이트버튼과 여전히 연결되어있다고 인식하고 있기 때문이다.
+     */
     var lightOn = true //toggle 방식을 위해 얻은 변수
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // 앱이 처음 시작할때 "button" 이라는 이름으로 시작된다. 이를 해결하기 위해
-        updateUI() // 이를 추가하면 된다. lightOn이 true인 상태이기 때문에, 흰색 배경으로 시작한다. 
+        updateUI() // 이를 추가하면 된다. lightOn이 true인 상태이기 때문에, 흰색 배경으로 시작한다.
     }
-
-
+    
+    
     @IBAction func buttonPressed(_ sender: Any) {
         
         lightOn = !lightOn //ON,OFF 방식이기때문에 누르면 현 누르기 전 상태의 반대가 나오도록 해야한다.
@@ -28,18 +32,8 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
-        if lightOn {
-            self.view.backgroundColor = .white //view의 배경색을 흰색으로 변경한다.
-            // button의 이름을 변경하는 함수
-            lightButton.setTitle("Off", for: .normal)
-        }else {
-            self.view.backgroundColor = .black
-            
-            lightButton.setTitle("Off", for: .normal)
-            // .normal의 뜻 : 어떤 버튼이.사용자의 이벤트를 기다리는 상태
-            // .highlighted : 사용자가 막 탭했을때가
-            // .disabled : 사용이 불가능한 상태
-        }
+        //tenary operator를 사용해서 코드를 간편화 하기
+        view.backgroundColor = lightOn ? .white : .black
     }
 }
 
